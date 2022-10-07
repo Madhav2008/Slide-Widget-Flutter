b3efa1d8b68864a9e2a5191d68f5b8cf29141083
arora <aroramadhav65@gmail.com>
2022-10-07 23:15:41 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -17,7 +17,8 @@ class _HomeScreenState extends State<HomeScreen> {
           padding: const EdgeInsets.all(12.0),
           child: SlideAction(
             borderRadius: 12,
-            elevation: 0,innerColor: Colors.deepPurple,
+            elevation: 0,
+            innerColor: Colors.deepPurple,
             onSubmit: () {
               // function after slide
             },
