11812eb5f0197af1d16e6fbdfdb55774c7bafd69
arora <aroramadhav65@gmail.com>
2022-10-07 22:59:06 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -11,7 +11,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: ,),
+      body: Center(child: Slider(value: value, onChanged: onChanged),),
     );
   }
 }
