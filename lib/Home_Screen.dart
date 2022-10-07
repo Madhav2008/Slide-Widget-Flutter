a4e2f21a0eb55dbaa44eba9523b1f1798f13646a
arora <aroramadhav65@gmail.com>
2022-10-07 23:16:21 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -19,7 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
             borderRadius: 12,
             elevation: 0,
             innerColor: Colors.deepPurple,
-            innerColor: Colors.deepPurple,
+            outerColor: Colors.deepPurple,
             onSubmit: () {
               // function after slide
             },
