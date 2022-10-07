c856a22633902af4ce582f47b0ab0a891147a5a5
arora <aroramadhav65@gmail.com>
2022-10-07 23:18:41 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -14,13 +14,13 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
       body: Center(
-        child: Padding(
+        child: Padding( 
           padding: const EdgeInsets.all(12.0),
           child: SlideAction(
             borderRadius: 12,
             elevation: 0,
             innerColor: Colors.deepPurple,
-            outerColor: Colors.deepPurple[200],
+            outerColor: Colors.deepPurple[200],sliderButtonIcon: ,
             onSubmit: () {
               // function after slide
             },
