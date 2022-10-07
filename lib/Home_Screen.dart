745cd8a60258823a1cc476ef243d2fd177f1c530
arora <aroramadhav65@gmail.com>
2022-10-07 23:19:57 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -21,7 +21,10 @@ class _HomeScreenState extends State<HomeScreen> {
             elevation: 0,
             innerColor: Colors.deepPurple,
             outerColor: Colors.deepPurple[200],
-            sliderButtonIcon: Icon(Icons.lock_open,color: Colors.white,),
+            sliderButtonIcon: Icon(
+              Icons.lock_open,
+              color: Colors.white,
+            ),
             onSubmit: () {
               // function after slide
             },
